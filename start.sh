65873d4c18306f3905a3d1d87625b793a008d881
Daniel Commins <dencee@users.noreply.github.com>
2024-02-24 13:03:04 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ else
 fi
 apt install x11-apps -y
 apt-get install xorg openbox
-export DISPLAY=:0
-setenv DISPLAY :0
+export DISPLAY=:1
+setenv DISPLAY :1
 go/bin/gotty -w java -jar memory-match.jar
